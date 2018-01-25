1fa1807d895b00001bd5a4386c9b0d2e8cc75795
Karol Głuch <31935271+gitesiek@users.noreply.github.com>
2018-01-25 17:29:58 +0100

    Update wyloguj.php

--- wyloguj.php
+++ wyloguj.php
@@ -1,6 +1,6 @@
 <?php
-session_start();
-$_SESSION['User_Login']=NULL;
-$_SESSION['User_ID']=NULL;
-header('Location: home.php');
+  session_start();
+  $_SESSION['User_Login']=NULL;
+  $_SESSION['User_ID']=NULL;
+  header('Location: home.php');
 ?>
