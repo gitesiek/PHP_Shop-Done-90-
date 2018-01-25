81aba81a92be756e7501c92e58e612737f066aba
Karol Głuch <31935271+gitesiek@users.noreply.github.com>
2018-01-25 17:24:22 +0100

    Update dodaj.php

--- dodaj.php
+++ dodaj.php
@@ -1,47 +1,47 @@
 <?php 
-session_start();
+	session_start();
 
-$connection=mysqli_connect('localhost','root','','uzytkownicy');
+	$connection=mysqli_connect('localhost','root','','uzytkownicy');
 
-$date=date("Y.m.d");
-$userID=$_SESSION['User_ID'];
+	$date=date("Y.m.d");
+	$userID=$_SESSION['User_ID'];
 
-$checkIfNew="SELECT ID, ClientID, Date FROM carts WHERE ClientID LIKE '%" . $_SESSION['User_ID'] . "%' AND Date(date) = CURDATE();";
+	$checkIfNew="SELECT ID, ClientID, Date FROM carts WHERE ClientID LIKE '%" . $_SESSION['User_ID'] . "%' AND Date(date) = CURDATE();";
 
-$insertNew="INSERT INTO carts (`ID`, `ClientID`, `Date`) VALUES (NULL,'$userID','$date');";
+	$insertNew="INSERT INTO carts (`ID`, `ClientID`, `Date`) VALUES (NULL,'$userID','$date');";
 
-$result=mysqli_query($connection, $checkIfNew); 
-	
-while($row=mysqli_fetch_array($result)){
-    $koszyk=$row['ID'];
-}
-$ItemID=$_GET['ItemID'];
-$quanity=$_GET['Quanity'];
+	$result=mysqli_query($connection, $checkIfNew); 
 
-$insertNewItem="INSERT INTO cartspositions (`ID`, `CartID`, `ProductID`, `Quanity`) VALUES (NULL, '$koszyk','$ItemID','$quanity');";
-
-$checkIfNewItem="SELECT CartID, ProductID FROM cartspositions WHERE CartID LIKE '%" . $koszyk . "%' AND ProductID LIKE '%" . $ItemID . "%';";
-
-if(empty($koszyk)){
-    mysqli_query($connection, $insertNew);
-    
-    $result=mysqli_query($connection, $checkIfNew); 
 	while($row=mysqli_fetch_array($result)){
-        $koszyk=$row['ID'];
-    } 
-}
+	    $koszyk=$row['ID'];
+	}
+	$ItemID=$_GET['ItemID'];
+	$quanity=$_GET['Quanity'];
 
-$result=mysqli_query($connection, $checkIfNewItem); 
-while($row=mysqli_fetch_array($result)){
-    $isNew=$row['ProductID'];
-} 
-if(empty($isNew)){
-    mysqli_query($connection,$insertNewItem);
-}
-else mysqli_query($connection, "UPDATE `cartspositions` SET `Quanity`= `Quanity`+ $quanity WHERE CartID LIKE '%" . $koszyk . "%' AND ProductID LIKE '%" . $ItemID . "%';");
-   
-echo "Pomyślnie dodano przedmiot do koszyka!".$koszyk;
-   
-header("Location: przedmioty.php");
+	$insertNewItem="INSERT INTO cartspositions (`ID`, `CartID`, `ProductID`, `Quanity`) VALUES (NULL, '$koszyk','$ItemID','$quanity');";
+
+	$checkIfNewItem="SELECT CartID, ProductID FROM cartspositions WHERE CartID LIKE '%" . $koszyk . "%' AND ProductID LIKE '%" . $ItemID . "%';";
+
+	if(empty($koszyk)){
+	    mysqli_query($connection, $insertNew);
+
+	    $result=mysqli_query($connection, $checkIfNew); 
+	    while($row=mysqli_fetch_array($result)){
+		$koszyk=$row['ID'];
+	    } 
+	}
+
+	$result=mysqli_query($connection, $checkIfNewItem); 
+	while($row=mysqli_fetch_array($result)){
+	    $isNew=$row['ProductID'];
+	} 
+	if(empty($isNew)){
+	    mysqli_query($connection,$insertNewItem);
+	}
+	else mysqli_query($connection, "UPDATE `cartspositions` SET `Quanity`= `Quanity`+ $quanity WHERE CartID LIKE '%" . $koszyk . "%' AND ProductID LIKE '%" . $ItemID . "%';");
+
+	echo "Pomyślnie dodano przedmiot do koszyka!".$koszyk;
+
+	header("Location: przedmioty.php");
 
 ?>
